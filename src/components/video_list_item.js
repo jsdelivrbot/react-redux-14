c7f1e08a8f38eedffa9e22cc9e2a40b190267d35
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 11:24:40 +1300

    Adding jsx/bootstrap markup to VideoListItem’s return method.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const VideoListItem = (props) =>{
-  return <li>Video</li>
+//Sugar. 'this component will recieve a prop with a key video. Please extract video from props, and assign to a variable called video'
+const VideoListItem = ({video}) =>{
+  console.log(video);
+  return(
+     <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+
+          </div>
+        </div>
+      </div>
+     </li>
+   );
 };
 
 export default VideoListItem;
