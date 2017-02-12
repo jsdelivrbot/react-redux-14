60d537cac09a5b840d20984ad20e79ca8e7854e8
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 10:18:39 +1300

    Inlining event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'; //Need to import react, because this c
 
 class SearchBar extends Component {
   render(){
-    return <input onChange={this.onInputChange}/>; //HTML elements emit a change event (vanilla html). Make event={reference to event handler}. Wrap javascript references inside curly braces. The thigns in the curly braces are a prop, or property.
-  }
-
-  onInputChange(event){ //on.   //Element watched.   //Event in element.  'event' is the event parameter being handled
-
+    return <input onChange={(event)=>console.log(event.target.value)}/>; //HTML elements emit a change event (vanilla html). Make event={reference to event handler}. Wrap javascript references inside curly braces. The thigns in the curly braces are a prop, or property.
   }
 }
 
