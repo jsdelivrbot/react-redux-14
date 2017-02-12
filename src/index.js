666b9fa4f2aa0b0254373ea9e4f4a2b78daded03
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 10:41:07 +1300

    Importing youtube-api-search. Doing dummy search.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBxJW5dxz_Nnv_TwhP83X3ZK9Pw6Is7TJw'
+
+YTSearch({key :API_KEY, term: 'surfboards'}, function(data) {console.log(data)});
 
 //Create a new component. This produces html
 const App = () =>{
