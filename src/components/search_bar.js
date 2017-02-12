36ad4a055b3ad918e19333dbf0d9840c0e567926
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 09:26:15 +1300

    Project now requires youtube-api-search npm install —save youtube-api-search
    Added SearchBar component, imported into index.js, and rendered in instantiated jsx. SearchBar is currently a functional component.

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react'; //Need to import react, because this component has jsx
+
+const SearchBar = () => {
+  return <input /> //This is jsx. Requires React to compile to React.create()
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBxJW5dxz_Nnv_TwhP83X3ZK9Pw6Is7TJw'
 
 //Create a new component. This produces html
-const App = function(){
-  return <div>Hi!</div>;
-}
+const App = () =>{
+  return ( //SearchBar is jsx for a React functional component, wrapped in tags to instantiate it.
+    <div>
+      <SearchBar />
+    </div> //Now that we have imported SearchBar, we can display it here
+  )
+};
+
 
 //Take the generated html, and put it on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
