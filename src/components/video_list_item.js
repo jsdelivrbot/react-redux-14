5c3c651680b01892c7585f8c287ff7f17da7f672
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 11:35:27 +1300

    Adding thumbnails and titles to VideoListItems.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 //Sugar. 'this component will recieve a prop with a key video. Please extract video from props, and assign to a variable called video'
 const VideoListItem = ({video}) =>{
-  console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url; //Pull image url from video variable. Can reference this variable inside jsx by wrapping variable name inside curlybrackets.
   return(
      <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imageUrl}/>
         </div>
 
         <div className="media-body">
           <div className="media-heading">
-
+            {video.snippet.title}
           </div>
         </div>
       </div>
