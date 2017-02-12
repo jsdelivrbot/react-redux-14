d7510751079ac4e8fb23470b8be2b3ccf01ebe32
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 10:47:53 +1300

    Moved YTSearch to app constructor. YT search sets state, storing the returned data list as the videos list.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBxJW5dxz_Nnv_TwhP83X3ZK9Pw6Is7TJw'
 
-YTSearch({key :API_KEY, term: 'surfboards'}, function(data) {console.log(data)});
 
 //Create a new component. This produces html
-const App = () =>{
-  return ( //SearchBar is jsx for a React functional component, wrapped in tags to instantiate it.
-    <div>
-      <SearchBar />
-    </div> //Now that we have imported SearchBar, we can display it here
-  )
-};
+class App extends Component{
+  constructor(props){
+    super(props);
 
+    this.state = {videos:[]};
+
+    YTSearch(
+      {key :API_KEY, term: 'surfboards'},
+      (data) => {this.setState({videos : data})}
+    );
+  }
+  render(){
+    return ( //SearchBar is jsx for a React functional component, wrapped in tags to instantiate it.
+      <div>
+        <SearchBar />
+      </div> //Now that we have imported SearchBar, we can display it here
+    );
+  }
+}
 
 //Take the generated html, and put it on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
