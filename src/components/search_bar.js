ba759b11505d51615680810e4986b84230e4d18f
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-13 10:16:53 +1300

    Example event handler for SearchBar input.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'; //Need to import react, because this c
 
 class SearchBar extends Component {
   render(){
-    return <input />;
+    return <input onChange={this.onInputChange}/>; //HTML elements emit a change event (vanilla html). Make event={reference to event handler}. Wrap javascript references inside curly braces. The thigns in the curly braces are a prop, or property.
+  }
+
+  onInputChange(event){ //on.   //Element watched.   //Event in element.  'event' is the event parameter being handled
+
   }
 }
 
